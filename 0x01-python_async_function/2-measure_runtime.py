9190b4e8d494b40a9eb1ee8b3062c828f8e5d4cd
Abdelrahman <boda8099@gmail.com>
2024-09-12 03:20:14 +0300

    test all

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """file doc for measuring time"""
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay)
     """
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
 
     total_time = end_time - start_time
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""  """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    task_wait_n function
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    dels = await asyncio.gather(*tasks)
+    nums = []
+
+    while dels:
+        small = min(dels)
+        nums.append(small)
+        dels.remove(small)
+    return nums  # storing the values here...
