aea5a9618d3de47a5612cf7e7595af5a89676375
Abdelrahman <boda8099@gmail.com>
2024-09-13 22:37:00 +0300

    arctic monkeys

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""what is the time
+"""
+from time import time
+import asyncio
+from asyncio import gather as gazzzzzzzzzzzzzzzzzzzz
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """excute async_com 4 times in parallel using gather
+    """
+    for i in range(4):
+        start = time()
+        await gazzzzzzzzzzzzzzzzzzzz(async_comprehension())
+        after = time()
+        return (after - start)
